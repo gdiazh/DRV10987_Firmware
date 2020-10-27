e2bd7a0faf401f0c24ded31ba6cf36eebf1c0149
gdiazh <g.hernan.diaz@gmail.com>
2020-10-26 23:29:54 -0300

    included intial speed for step test

--- ros_uart_controller/uart_speed_step.py
+++ ros_uart_controller/uart_speed_step.py
@@ -14,13 +14,16 @@ class DRV10987Controller:
     def __init__(self, port, baud, folder, test_name, test_params, test_specs):
         # Only argument stuff
         self.test_specs = test_specs
-        self.serial_trx = serialInterface(port, baud, debug = True)
+        self.serial_trx = serialInterface(port, baud, debug = False)
         self.file_handler = fileHandler(folder, test_name)
-        self.cmd_code = 0                    #[cmd]
-        self.speed = 0                       #[rpm]
-        self.stop_time = test_params[0]      #[s]
-        self.step_value = test_params[1]     #[cmd]
-        self.coasting_time = test_params[2]  #[s]
+        self.cmd_code = 0                                   #[cmd]
+        self.speed = 0                                      #[rpm]
+        self.steady_time = 5                                #[s]
+        self.finalcoasting_time = 3                         #[s]
+        self.stop_time = test_params[0]+self.steady_time    #[s]
+        self.initial_value = test_params[1]                 #[cmd]
+        self.step_value = test_params[2]                    #[cmd]
+        self.coasting_time = test_params[3]                 #[s]
 
     def initialize(self):
         # Get params and allocate msgs
@@ -35,9 +38,22 @@ class DRV10987Controller:
         initial_time = timer()              #[s]
         time = timer() - initial_time       #[s]
         step_stoped = False
-        # Send speed cmd
-        self.serial_trx.write(1, int(self.step_value))
-        while time < self.stop_time + self.coasting_time:
+        ST1 = True
+        ST2 = True
+        ST3 = True
+        while time < self.stop_time + self.finalcoasting_time + self.coasting_time:
+            if 0 <= time and ST1:   # Send initial speed cmd value
+                print(chr(27)+"[1;31m"+"Initial Speed CMD: "+str(self.initial_value)+chr(27)+"[0m")
+                self.serial_trx.write(1, int(self.initial_value))
+                ST1 = False
+            elif self.steady_time <= time and ST2: # Send step speed cmd value
+                print(chr(27)+"[1;31m"+"Step Value CMD: "+str(self.step_value)+chr(27)+"[0m")
+                self.serial_trx.write(1, int(self.step_value))
+                ST2 = False
+            elif self.stop_time <= time and ST3: # Send final step speed cmd value
+                print(chr(27)+"[1;31m"+"Final Step Value CMD: "+str(self.initial_value)+chr(27)+"[0m")
+                self.serial_trx.write(1, int(self.initial_value))
+                ST3 = False
             if(self.serial_trx.read()):
                 # read data
                 data = self.serial_trx.struct 
@@ -53,9 +69,10 @@ class DRV10987Controller:
                 self.file_handler.historical_current_ma.append(current_ma_1)
                 self.file_handler.historical_speed_rpm.append(speed_rpm_1)
                 self.file_handler.historical_speed_cmd.append(speed_cmd_1)
-                if time > self.stop_time and not(step_stoped):
-                    self.serial_trx.write(1, 0)
-                    step_stoped = True
+            if time > self.stop_time + self.finalcoasting_time and not(step_stoped):
+                print(chr(27)+"[1;31m"+"Stopping CMD: "+str(0)+chr(27)+"[0m")
+                self.serial_trx.write(1, 0)
+                step_stoped = True
         self.serial_trx.stop()
         print(chr(27)+"[1;31m"+"Test Finished"+chr(27)+"[0m")
 
@@ -67,18 +84,19 @@ if __name__ == '__main__':
     test_name = input("test_name: ")
     if test_name=="dummy":
         folder = "data/dummy/"
-        test_params = [8, 100, 2]
+        test_params = [8, 0, 100, 2]
         test_specs = None
     else:
         print("Specify Test conditions")
         default_conf = input(chr(27)+"[0;33m"+"Used default configuration (used only if you are sure) y/n?: "+chr(27)+"[0m")
         if default_conf == "y":
-            folder = "data/steps/"
+            folder = "data/inisteps/"
             brief = "Brief: Speed step input to drv through uart"
             motor_type = "Motor type (dvd/HDD/Other): dvd"
             flywheel_type = "Flywheel type (medium/rods/small/No): No"
             motor_alignment = "Motor Rot. Axis alignment (vertical/horizontal): horizontal"
-            stop_time = "8"
+            stop_time = "7"
+            initial_value = "100"
             step_value = input("Step value[cmd](Sugested > 60): ")
             coasting_time = "2"
         else:
@@ -88,14 +106,16 @@ if __name__ == '__main__':
             flywheel_type = "Flywheel type (medium/rods/small/No): " + input("Flywheel type (medium/rods/small/No): ")
             motor_alignment = "Motor Rot. Axis alignment (vertical/horizontal): " + input("Motor Rot. Axis alignment (vertical/horizontal): ")
             stop_time = input("Stop time[s](Sugested > 7): ")
+            initial_value = input("Initial step value[cmd](Sugested > 60): ")
             step_value = input("Step value[cmd](Sugested > 60): ")
             coasting_time = input("Coasting time[s](Sugested > 1): ")
-        test_params = [float(stop_time), float(step_value), float(coasting_time)]
+        test_params = [float(stop_time), float(initial_value), float(step_value), float(coasting_time)]
         test_specs = [brief,
                       motor_type,
                       flywheel_type,
                       motor_alignment,
                       "Stop time[s]: " + stop_time,
+                      "Initial step value[cmd]: " + initial_value,
                       "Step value[cmd]: " + step_value,
                       "Coasting time[s]: " + coasting_time]
         if default_conf == "y":
